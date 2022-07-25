9f45f3aa62eff6494b74a3ec7b075ffc5b524876
El houcine Oulmostaf <houcinolmostaf@example.com>
2022-07-25 14:21:03 -0700

    gg

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
+
 
 import DrawerNavigator from './App/Navigations/Drawer';
 
